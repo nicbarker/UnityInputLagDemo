35405629752d6daa744bbe797bb7a65b402fcf3f
Nic Barker <nic.b@canva.com>
2020-02-01 16:05:45 +1300

    Initial commit

---
+++ Assets/ClickBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickBehaviour : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) {
+            sprite.color = new Color(0,0,0,1);
+        } else {
+            sprite.color = new Color(1,1,1,1);
+        }
+    }
+}
